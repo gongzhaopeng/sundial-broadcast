8203144ac6482c80cce24ca97f68fc1e9efc316e
gongzhaopeng <gongguochuanqi@gmail.com>
2020-12-22 13:59:06 +0800

    ...

--- src/test/java/cn/benbenedu/sundial/broadcast/CreditEaseSignTests.java
+++ src/test/java/cn/benbenedu/sundial/broadcast/CreditEaseSignTests.java
@@ -3,14 +3,11 @@ package cn.benbenedu.sundial.broadcast;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class CreditEaseSignTests {
 
-    private static final String SIGN_KEY = "96388dd2f29d13370b9a3cb488cbe128acd6c70b45e527ca185423b4972600c4";
+    private static final String SIGN_KEY = "7be47baf24d901787b6148d23661f7745e2ae156d00b2cab111913dd4ffe9ce1";
 
     @Test
     public void notificationSign() {
@@ -23,6 +20,18 @@ public class CreditEaseSignTests {
                 "assessResultUrl", "https://reports.benbenedu.cn/personalStatusYixin_v1/report_5fb23100ec198920eaa32888_20201116081211.pdf");
         final var token = sign(params, timestamp);
         System.out.println("notificationSign.token: " + token);
+    }
+
+    @Test
+    public void acquiringSign() {
+
+        final var timestamp = 1608612665075L;
+        final var params = new HashMap<String, Object>();
+        params.put("productId", "QSNHXSZ");
+        params.put("assessCode", "9X6fMuALXdNX");
+        params.put("orderCode", null);
+        final var token = sign(params, timestamp);
+        System.out.println("acquiringSign.token: " + token);
     }
 
     public String sign(final Map<String, Object> params,
